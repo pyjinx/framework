cac656fdb4ccd15948f71aa01a0e3d0cab50ebf1
Krunal Dodiya <kunal.dodiya1@gmail.com>
2026-08-20 09:16:40 +0530

    Remove application skeleton from framework package

--- pyjinx/pyjinx/routes/web.py
+++
@@ -1,8 +0,0 @@
-from app.http.controllers.TodoController import TodoController
-
-
-def register_routes(app):
-    """Register HTTP routes for the starter app."""
-
-    router = app.make("router")
-    router.post("/todos", [TodoController, "index"])
