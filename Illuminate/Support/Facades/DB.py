900670a9e28fb7c2358159f54031a5f4216e813f
Krunal Dodiya <kunal.dodiya1@gmail.com>
2026-08-20 17:45:47 +0530

    Add static typing for DB facade

--- Illuminate/Support/Facades/DB.py
+++ Illuminate/Support/Facades/DB.py
@@ -1,7 +1,22 @@
+from typing import TYPE_CHECKING
+
 from Illuminate.Support.Facades.Facade import Facade
+
+if TYPE_CHECKING:
+    from Illuminate.Database.QueryBuilder import QueryBuilder
 
 
 class DB(metaclass=Facade):
     @classmethod
     def get_facade_accessor(cls):
         return "db"
+
+    if TYPE_CHECKING:
+
+        @classmethod
+        def table(
+            cls,
+            table_name: str,
+            connection_name: str | None = None,
+        ) -> "QueryBuilder":
+            ...
